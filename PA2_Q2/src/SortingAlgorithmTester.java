d231b09a9ff084d7c0cb084d78ac98483d28f397
SKT1803 <skemalt@outlook.com>
2025-05-27 20:33:01 +0300

    Update SortingAlgorithmTester.java

--- PA2_Q2/src/SortingAlgorithmTester.java
+++ PA2_Q2/src/SortingAlgorithmTester.java
@@ -1,6 +1,6 @@
 //-----------------------------------------------------
 // Title: SortingAlgorithmTester Class
-// Author: Serdar Kemal Topkaya
+// Author: SKT
 // Description: In this class, we create three arrays of three different types.(Random, increasing, decreasing)
 // After that, we are making experimental run time analysis of these arrays in the four different settings 
 // where in each setting we are going to use different sorting algorithms (Selection, Insertion, Merge, Quick)
